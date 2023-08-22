c862f5015f09b77a763be7b41f9b151158ecd9a6
Dikshen Makwana <makwanadikshen@gmail.com>
2023-08-22 20:41:49 +0530

    Add files via upload
    Sign Up page skeleton made

---
+++ signup.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Up</title>
+</head>
+<body>
+    <form action="">
+        <label for="uname">UserName</label>
+        <br>
+        <input type="text" placeholder="Enter your username" id="uname" required>
+        <br>
+        <label for="email">Enter your E-mail address</label>
+        <br>
+        <input type="email" placeholder="Enter your e-mail" id="email" required>
+        <br>
+        <label for="pcode">Enter the password</label>
+        <br>
+        <input type="password" placeholder="Enter the password" id="pcode" required>
+        <br>
+        <input type="checkbox" required>
+        <label for="cbox">I agree to all the <a href="#" >Terms and Conditions</a></label>
+        <br>
+        <input type="submit">
+    </form>
+</body>
+</html>
